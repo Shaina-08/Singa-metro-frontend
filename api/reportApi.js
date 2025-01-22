bc50ebad5de689a1216dd251b126f635705348f1
S3b2 <111647190+Shaina-08@users.noreply.github.com>
2025-01-22 13:18:07 +0530

    Singa Metro fare calculator

--- api/reportApi.js
+++ api/reportApi.js
@@ -1,2 +1,2 @@
-export const API_BASE_URL = "https://22e2-2a09-bac5-3f09-a82-00-10c-2d.ngrok-free.app/api/";
+export const API_BASE_URL = "https://22e2-2a09-bac5-3f09-a82-00-10c-2d.ngrok-free.app/api";
 
